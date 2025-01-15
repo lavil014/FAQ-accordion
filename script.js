d22ced7b6562a1c384db8a1479d2bfaf3898db8c
Luis Aviles <123853326+lavil014@users.noreply.github.com>
2025-01-14 20:46:16 -0600

    New update made to my script file

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const btn = document.getElementById('plus-icon');
+const btn = document.getElementById('icon');
 const paragraph = document.getElementById('paragraph');
+let iconPlus = 'http://127.0.0.1:5500/assets/images/icon-plus.svg';
+let iconMinus = 'http://127.0.0.1:5500/assets/images/icon-minus.svg';
+
+
 
 console.log(btn, paragraph)
 
-btn.addEventListener('click', ()=>{
+btn.addEventListener('click', (e)=>{
     paragraph.style.visibility = 'visible';
+
+    console.log(iconPlus);
+
 })
+
