17e60070ab95cac68b2828d60bfb0514ea227860
Luis Aviles <123853326+lavil014@users.noreply.github.com>
2025-01-15 20:55:13 -0600

    New changes made to HTML and Script files

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-const btn = document.getElementById('icon');
-const paragraph = document.getElementById('paragraph');
+const btns = document.querySelectorAll('img');
 let iconPlus = 'http://127.0.0.1:5500/assets/images/icon-plus.svg';
 let iconMinus = 'http://127.0.0.1:5500/assets/images/icon-minus.svg';
 
 
 
-console.log(btn, paragraph)
+console.log(btns.length)
 
-btn.addEventListener('click', (e)=>{
-    paragraph.style.visibility = 'visible';
+btns.forEach((btn)=>{
+    btn.addEventListener('click',(e)=>{
+        
 
-    console.log(iconPlus);
-
+        if(btn.src === iconPlus){
+            btn.src = iconMinus;
+            paragraph.style.visibility = 'visible';
+        } else{
+            btn.src = iconPlus;
+            paragraph.style.visibility = 'hidden';
+        }
+    })
 })
+
 
