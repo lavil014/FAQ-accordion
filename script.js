0212a616e5dae327d5493bd6a339ff932226d025
Luis Aviles <123853326+lavil014@users.noreply.github.com>
2025-01-17 22:23:54 -0600

    Media query added to my stylesheet

--- script.js
+++ script.js
@@ -3,22 +3,23 @@ let iconPlus = 'icon-plus.svg';
 let iconMinus = 'icon-minus.svg';
 
 
-
 btns.forEach((btn)=>{
 
     btn.addEventListener('click',()=>{
 
         const pContent = btn.nextElementSibling;
+        let headings = document.querySelectorAll('.headings');
 
+        let faqItem = btn.parentElement;
+        let paragraphs = faqItem.nextElementSibling;
 
         if(btn.src.includes(iconPlus)){
             btn.src = btn.src.replace(iconPlus, iconMinus);
-            pContent.style.display = 'flex';
+            paragraphs.style.display = 'flex';
             
         } else if(btn.src.includes(iconMinus)){
             btn.src = btn.src.replace(iconMinus,iconPlus);
-            pContent.style.display = 'none';
-           
+            paragraphs.style.display = 'none';
         }
     })
 })
